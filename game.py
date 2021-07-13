17a840c68e9d44d1b624ecc4cdb9db62a058de42
gbacila <32124391+gbacila@users.noreply.github.com>
2021-07-13 16:30:43 -0400

    Added input statement

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
 # game.py
-
+options = ["Rock","Paper","Scissors"]
 print("Rock, Paper, Scissors, Shoot!")
+
+#Getting the user's inputs
+
+game = input("Please enter Rock, Paper or Scissors here: ")
+
+#Checing the inputs
+
+
+print(game)
