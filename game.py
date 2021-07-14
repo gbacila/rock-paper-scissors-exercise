bf7e60b2f17f71d42c1d930cf430a19f59fca4c2
gbacila <32124391+gbacila@users.noreply.github.com>
2021-07-14 16:30:36 -0400

    Final version of the game with comments

--- game.py
+++ game.py
@@ -1,33 +1,41 @@
 # game.py
 
+#Importing all necessary modules and packages
 import random
 from dotenv import load_dotenv
 import os
+
+
+#Creating the list of options for further reference
 options = ["Rock","Paper","Scissors"]
 
+# Pulling the username info from .env and using it in the welcome message
 load_dotenv()
-#print(os.getenv("SECRET_MESSAGE"))
-
 usname = os.getenv("USER_NAME")
-print(usname)
+message = f"Welcome to the game, {usname}!"
+print(message)
 
-print("Welcome to the game, ",usname,"!")
-print("Rock, Paper, Scissors, Shoot!")
-#Getting the user's inputs
-game = input("Please enter Rock, Paper or Scissors here: ")
-
+#Getting the inputs from the user
+game = input("To play the game, please enter Rock, Paper or Scissors here: ")
 game = game.title()
 
-#Checing the inputs
+#Validating the inputs
 if game == "Rock" or game == "Paper" or game == "Scissors":
-    print("The player selected ",game)
+    print("The input is valid. You selected ",game)
 else:
     print("The input is incorrect, exiting")
     exit()
 
+#Generating computer's choice from the list of options
 comp = random.choice(options)
 print("Computer selected: ",comp)
-# -1
+
+#In this section we determine the winner by sending the program through a nested set of IF
+#statements. The idea is to make sure the program doesn't have to go through each and every
+#IF statement for all 9 potential outcomes, and only goes through one "branch" of the
+#conditional. This should help with calculating the results faster.
+#When the program goes through the conditionals, it determines if the player won (x=1),
+#lost (x=-1) or if it's a draw (x=0).
 if game == "Rock":
     if comp == "Rock":
         x = 0
@@ -46,11 +54,16 @@ else:
     elif comp == "Paper":
         x = 1
     else: x = -1
+
+#Now that we know the result (x being -1, 1, or 0), we can generate the outcome message.
+#By using this second IF statement we reduce "wordiness" of the code because we don't need
+#to have those Print statement at every outcome above - we only list them once here.
 if x == 0:
     print("Draw!")
 elif x == -1:
     print("You lost, try again")
 else: print("You won! Congrats")
 
-print("It was a great game, ",usname,", Please play again!")
-#new_message =f"Hello {var} World"
+#The game is over and it's time to say goodbye.
+message = f"It was a great game, {usname}, please play again!"
+print(message)
